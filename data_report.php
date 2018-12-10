8e3299020382e89721d3280b3eee5283b1656324
Ved <veddandekar6@gmail.com>
2018-12-11 02:51:44 +0530

    Fixed rookie mistake

--- data_report.php
+++ data_report.php
@@ -7,7 +7,7 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 
 		<title>
-			Add Record
+			Data Report
 		</title>
 		<link rel="stylesheet" href="css/bootstrap.min.css">
 		<link rel="stylesheet" href="css/register.css">
