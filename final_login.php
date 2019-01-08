2be51208681191319301fa2b2abd0781e54896e4
Yash Shah <yashah1234@gmail.com>
2019-01-08 13:26:15 +0530

    removed useless js and css imports in final_login

--- final_login.php
+++ final_login.php
@@ -82,10 +82,8 @@ if(isset($_POST['username']) && isset($_POST['password'])){
 		<title>Publication Data Repo Login</title>
 			<link rel="stylesheet" href="css/menu.css">
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-		<link rel="stylesheet" href="css/login.css">
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-		<script src=js/authentication.js> </script>
 	</head>
 	<body class="login">
 	
